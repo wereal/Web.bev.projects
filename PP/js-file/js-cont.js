20f75909ff1e284b8d456ca6c46886afec8d1867
wereal <cs04061@uowm.gr>
2023-01-05 21:20:17 +0200

    sml update
    nav bar over content
    some small text updates

--- PP/js-file/js-cont.js
+++ PP/js-file/js-cont.js
@@ -6,7 +6,7 @@ const navSlide = () => {
     //toggle navigation
     burger.addEventListener('click', () => {
       nav.classList.toggle('nav-active');
-  
+     // document.body.classList.toggle('overlay');
       // animate links
       navLinks.forEach((link, index) => {
         if (link.style.animation) {
@@ -14,7 +14,10 @@ const navSlide = () => {
         } else {
           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
         }
+
+
       });
+        
     });
   };
   
