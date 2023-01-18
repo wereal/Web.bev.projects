8201f0887890595a39216787135cc5350a38fb84
Marios Iosifidis <94121668+wereal@users.noreply.github.com>
2023-01-18 04:54:25 +0200

    Delete miscellaneous directory

--- miscellaneous/Nav_bar/js.js
+++
@@ -1,9 +0,0 @@
-
-  function toggleNav() {
-    const navLinks = document.querySelector(".nav_links");
-    const burger = document.querySelector(".burger");
-    navLinks.style.display = navLinks.style.display === "none" ? "block" : "none";
-    burger.classList.toggle("change");
-  }
-
-  toggleNav();
